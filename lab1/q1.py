bd77e450eba998f8d2cce13aa1c9294fa6d761b1
dalvimangesh <dalvimangesh000@gmail.com>
2023-01-28 15:25:44 +0530

    q1

--- lab1/q1.py
+++ lab1/q1.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 
-inf = 1000000 # Assuming 1000000 as -infinity
+inf = 1000000  # Assuming 1000000 as infinity
 
+
+# function finds the log of actual factorials using the math library
 def findLogActualFactorials(n):
 
     # storing the log of actual factorials
@@ -17,15 +19,19 @@ def findLogActualFactorials(n):
     return ActuralFactorials
 
 
+
+# function finds the log of stirling's approximation
 def findLogWithStirling(n):
 
     # storing the log of Stirling Approximation
-    logStirling = [-inf]  
+    logStirling = [-inf]
 
+
+    # This function finds the log of stirling's approximation for a input number
     def findLogStirling(num) -> float:
-        val = (math.log(2*math.pi)/2) + ((math.log(i))
-                                         * (1/2 + i)) - (i * math.log(math.e))
+        val = (math.log(2*math.pi)/2) + ((math.log(i))* (1/2 + i)) - (i * math.log(math.e))
         return val
+
 
     for i in range(1, n+1):
         val = findLogStirling(i)
@@ -34,8 +40,7 @@ def findLogWithStirling(n):
     return logStirling
 
 
-
-
+# function finds the difference between the each element of the two lists 
 def findDiff(list1, list2):
     n = len(list1)
     difference = []
@@ -51,12 +56,13 @@ if __name__ == '__main__':
     ActuralFactorials = findLogActualFactorials(noOfExpt)
     logStirling = findLogWithStirling(noOfExpt)
 
-    # storing differences in actual and Stirling Approximation
+    # storing differences in actual and Stirling Approximation for each number
     difference = findDiff(ActuralFactorials, logStirling)
 
+
+    # Plotting the graph for log values of actual factorials and stirlings approximation
     plt.plot(ActuralFactorials, 'r', label='log(n!)', linewidth='1.5')
-    plt.plot(logStirling, 'b:',
-             label='log ( Stirling Approximation  for n)', linewidth='3.5')
+    plt.plot(logStirling, 'b:',label='log ( Stirling Approximation  for n)', linewidth='3.5')
     plt.xlabel('n')
     plt.ylabel('log values')
     plt.title('Stirlings approximation')
@@ -64,8 +70,8 @@ if __name__ == '__main__':
     plt.grid()
     plt.show()
 
-    plt.plot(difference, color='tab:red',
-             label=' log(n!) - log( Stirling Approximation for n ) ', linestyle='solid')
+    # Plotting the graph for the difference between log values of actual factorials and stirlings approximation
+    plt.plot(difference, color='tab:red', label=' log(n!) - log( Stirling Approximation for n ) ', linestyle='solid')
     plt.ylim(-0.000003, 0.000003)
     plt.axhline(y=0, color='tab:blue', label='zero line')
     plt.xlabel('n')
