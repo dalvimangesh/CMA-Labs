b44fc7847e5ec7da3d1c175a5cbc105976889c5f
dalvimangesh <dalvimangesh000@gmail.com>
2023-02-07 12:17:10 +0530

    q3

--- lab2/q2.py
+++ lab2/q2.py
@@ -1,17 +1,28 @@
 from q1 import UndirectedGraph
 from random import random
+import sys
 
-# derived class from UndirectedGraph Class to create a Erdos-Renyi random graph G(n, p)
+# derived class from UndirectedGraph Class to create a Erdos-Renyi random graph
 class ERRandomGraph(UndirectedGraph):
 
     def __init__(self, numVertices=None) -> None:
         super().__init__(numVertices)
 
+    # Generates a random graph with given probability p
     def sample(self, probability):
-        
+
+        try:
+            if probability < 0 or probability > 1:
+                raise Exception('Probability should be non negative and less than equal to 1')
+        except Exception as e:
+            print(type(e))
+            print(e)
+            sys.exit(-1)
+
         for u in range(1, self.numVertex+1):
             for v in range(u+1, self.numVertex+1):
                 randNum = random()
+                # adding edge with given probability
                 if (randNum < probability):
                     self.addEdge(u, v)
 
