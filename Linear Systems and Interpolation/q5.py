305bd55bee7332688a16af247b01117b3e8facb8
dalvimangesh <dalvimangesh000@gmail.com>
2023-02-15 20:41:48 +0530

    q5

--- Linear Systems and Interpolation/q5.py
+++ Linear Systems and Interpolation/q5.py
@@ -10,26 +10,27 @@ def true(x):
 # Define the function that will update the plot for each frame of the animation
 def update(frameNum):
     
-    xPoints = np.linspace(-3, 3, frameNum)
+    xPoints = np.linspace(-5, 5, frameNum)
     yPoints = true(xPoints)
 
     # Calculating the true function values for the x-axis
     yOfTrue = true(x)
 
     # Update the lines with the interpolated values for this frame
+
     cubicLine.set_data(x, CubicSpline(xPoints, yPoints)(x) )
     akimaLine.set_data(x, Akima1DInterpolator(xPoints, yPoints)(x) )
     baryLine.set_data(x, BarycentricInterpolator(xPoints, yPoints)(x))
 
     trueLine.set_data(x, yOfTrue)
-    ax.set_title(f'Different interpolations of "(np.tan(x) * np.sin(30 * x)) * np.exp(x)" for {frameNum/5} samples')
+    ax.set_title(f'Interpolations of tan(x)*sin(30*x)*exp(x), Frame : {frameNum/5}')
 
 
 if __name__ == '__main__':
 
     x = np.linspace(0, 1, 10000) # Generate 10000 points between (0,1)
 
-    points = np.arange(5, 200, 5) # Generate a list of the number of points to sample for the animation
+    points = np.arange(5, 300, 5) # Generate a list of the number of points to sample for the animation
 
     fig, ax = plt.subplots() # figure and axes for the animation
 
@@ -46,5 +47,5 @@ if __name__ == '__main__':
     ax.set_xlim(0, 1)
     ax.set_ylim(-4, 4)
     animation = FuncAnimation(fig, update, frames=points)
-    animation.save('q5.gif')
+    # animation.save('q5.gif')
     plt.show() # result
